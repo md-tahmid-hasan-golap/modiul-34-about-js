80eb9f3359fc17665320b7c47ae7271524fb7acb
golap-raj <golapraj47@gmail.com>
2025-03-11 15:52:48 +0600

    add j

---
+++ scripts/index.js
@@ -0,0 +1,53 @@
+// console.log("hero")
+// console.log("Alom")
+// console.log("Hena")
+
+// function person (){
+//     console.log("Golap")
+// }
+
+// person()
+
+// const person1 = () => {
+//     console.log("tahmid")
+// // }
+
+// // console.log(person1)
+
+
+// const a = 10;
+
+// const first = ()=> {
+//     secend()
+//     console.log("I am a First")
+// }
+// const secend = ()=> {
+//     third()
+//     console.log("I am a secend")
+// }
+// const third = ()=> {
+//     console.log("I am a third")
+// }
+
+// const b = 20;
+// first()
+// console.log("ami sesh a achi")
+
+const a = 10; 
+const b = 20;
+
+
+console.log("ami prothom")
+
+
+fetch ("https://jsonplaceholder.typicode.com/todos/1")
+.then(res => res.json())
+.then(data => console.log(data))
+
+console.log("ami secend")
+const result = a + b;
+console.log(result)
+
+// ata ekta function
+
+setTimeout ()
